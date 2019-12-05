eaddb76635c678935fe6621da2e48bf8cd704951
Terazus <batistadominique@hotmail.com>
2019-12-05 01:38:36 -0800

    starting tuto

---
+++ src/components/GeneFeature.js
@@ -0,0 +1,61 @@
+/** Class to create and manipulate small gene features
+* @param {String} name - The name of the feature
+* @param {String} sequence - The sequence of the feature
+*/
+
+import {codons} from './codon.js';
+
+export class GeneFeature {
+
+	constructor(name, sequence) {
+		let _name = name;
+		let _sequence = sequence;
+
+		/** A mapping object between the codons and the corresponding protein sequence */
+		this.mapping = codons;
+
+		/** Getter for the feature name
+		 * @returns {String} name
+		 */
+		this.getName = function(){
+			return _name;
+		};
+
+		/** Getter for the feature sequence
+		 * @returns {String} sequence
+		 */
+		this.getSequence = function(){
+			return _sequence;
+		};
+	}
+
+	/** This method transcribes the feature by replacing Ts with Us
+	 * @returns {String} sequence - the RNA sequence transcribed from the DNA sequence
+	 */
+	transcribe(){
+		return this.getSequence().replace(/T/g, "U");
+	};
+
+	/** This method translate the transcribe into a protein primary sequence.
+	 * @returns {string} translate - the protein primary sequence translated from that DNA feature
+	 */
+	translate(){
+		let translated = "";
+
+		let Gene = this;
+		let sequence_array = Gene.transcribe().match(/.{1,3}/g);
+		sequence_array.forEach(function(seq){
+			if (seq.includes("X")){
+				throw 'X is unsupported at the moment';
+			}
+			if (Gene.mapping[seq] === "Stop" || seq.length < 3){
+				return translated;
+			}
+			if (Gene.mapping[seq] === undefined){
+				throw "Undefined codon in the input sequence";
+			}
+			translated += Gene.mapping[seq];
+		});
+		return translated;
+	}
+}
